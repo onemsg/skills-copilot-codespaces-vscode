6a794a2bb0cf56fbf69182852e2316e35720d001
徽章 <42440341+onemsg@users.noreply.github.com>
2023-07-18 04:35:41 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,7 @@
+function skillsMember() {
+  if (member.value == "1") {
+    document.getElementById("member").style.display = "block";
+  } else {
+    document.getElementById("member").style.display = "none";
+  }
+}
